6fbc170c82817a2ccc4ced7a81566f5307a28f58
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-05-25 21:28:31 -0300

    criação APIs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,41 @@
-const person = 'Carla Lucio';
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
+const categories: any = [];
 
 
-const sayHello = (name: string) => {
-  console.log(`Hello, ${name}`);
-};
+app.get('/', (request: Request, response: Response) => {
 
-sayHello(person);
+  return response.json({ status: 'success', version: '1.0.0'}).status(200)
+});
+
+app.get('/categories', (request: Request, response: Response) => {
+  return response.json(categories).status(200)
+});
+
+app.post('/categories', (request: Request, response: Response) => {
+  const data = request.body;
+
+  const category = {
+  id: (categories.length + 1) ,
+  name: data.name,
+  created_at: new Date(),
+  update_at: new Date(),
+  }
+
+  categories.push(category);
+  return response.json().status(200)
+});
+
+app.get('/categories/:id', (request: Request, response: Response) => {
+  const {id} = request.params;
+  const category = categories.find((item: any) => item.id ==id)
+
+  return response.json(category).status(200)
+});
+
+
+app.listen(3000, () => {
+   console.log('Server is running');
+});
