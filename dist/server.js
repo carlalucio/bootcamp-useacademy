184df5a3f8e7243c806e97ef25580919e5d8a39c
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-05-31 15:54:49 -0300

    deploy

--- dist/server.js
+++ dist/server.js
@@ -8,20 +8,18 @@ const routes_1 = require("./routes");
 const environment_variables_1 = require("./config/environment-variables");
 const data_source_1 = require("./config/data-source");
 const middlewares_1 = require("./middlewares");
+const path_1 = require("path");
 const PORT = environment_variables_1.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 //usar o arquivo de rotas
 app.use(routes_1.routes);
 app.use(middlewares_1.errorHandler);
-app.use('/files', express_1.default.static(resolve(__dirname, '..', 'uploads')));
+app.use('/files', express_1.default.static((0, path_1.resolve)(__dirname, '..', 'uploads')));
 //Cria uma porta e inicializar o banco de dados
 data_source_1.AppDataSource.initialize()
     .then(() => {
     app.listen(PORT, () => console.log(`Server in running in port ${PORT}`));
 })
     .catch((error) => console.log(error));
-function resolve(_dirname, arg1, arg2) {
-    throw new Error('Function not implemented.');
-}
 //# sourceMappingURL=server.js.map
